fe4f6c0c436b2299ddad06c79b2920a5ed096f34
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-05-25 13:50:41 -0500

    basic js layout

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var questions = [
+
+    {
+        question:"which is not a JS data type"
+        choices:['...'.'...','....','...']
+        answer: '...'
+
+    },
+
+    {
+        question:"which is not a JS data type"
+        choices:['...'.'...','....','...']
+        answer: '...'
+
+    },
+
+]
+
+questions[currentQuestion].question
+questions[currentQuestion].choices
+questions[currentQuestion].answer
