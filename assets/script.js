dc0bc89d70d6f4bd7cad4b30130a292c7677e62f
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-05-25 14:31:06 -0500

    added complete questions, some variables

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,31 @@
 var questions = [
 
     {
-        question:"which is not a JS data type"
-        choices:['...'.'...','....','...']
-        answer: '...'
+        question:"Commonly used data types DO NOT include:"
+        choices:['1. Strings','2. Booleans','3. Alerts','4. Numbers']
+        answer: '4. Alerts'
 
     },
 
     {
-        question:"which is not a JS data type"
-        choices:['...'.'...','....','...']
-        answer: '...'
+        question:"The condition in an if / else statement is enclosed with:"
+        choices:['1. Quotes','2. Curly brackets','3. Parenthesis','4. Square brackets']
+        answer: '3. Parenthesis'
 
     },
 
+    {
+        question:"Arrays in javascript can be used to store:"
+        choices:['1. Numbers and strings','2. Other arrays','3. Booleans','4. All of the above']
+        answer:'4. All of the above'
+    }
+
 ]
+
+var quizContainter = document.getElementById('quiz-container');
+var startButton = document.getElementById('start');
+
+
 
 questions[currentQuestion].question
 questions[currentQuestion].choices
